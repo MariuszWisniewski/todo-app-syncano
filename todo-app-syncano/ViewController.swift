fce3328cdd51073eeab3c6b788365fcd9d169a80
Mariusz Wisniewski <lifcio@gmail.com>
2015-08-13 18:18:11 -0400

    Empty project

---
+++ todo-app-syncano/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  todo-app-syncano
+//
+//  Created by Mariusz Wisniewski on 8/13/15.
+//  Copyright (c) 2015 Mariusz Wisniewski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
